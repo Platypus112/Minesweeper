5727b057ec2277344d44766d6b3b03f11865e704
Platypus112 <idancar7@gmail.com>
2024-08-17 18:10:07 +0300

    Add project files.

---
+++ Minesweeper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Minesweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Tile(4,4,1));
+        }
+    }
+}

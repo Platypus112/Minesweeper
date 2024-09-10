16bdac46f37f68619a1825ef535a13a8deb6c64f
AC1231231 <amitchacham1@gmail.com>
2024-09-10 13:00:19 +0300

    grid

---
+++ Minesweeper/Game.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class Game
+    {
+        private readonly Tile[,] Board;
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }   
+        public bool IsPlaying { get; private set; }
+        public bool? GameWon { get {return WonGame(); }  }
+        public int Bombs {  get; private set; }
+        public Game(int height,int width,int Bombs_)
+        {
+            StartTime=DateTime.Now;
+            EndTime=DateTime.Now;
+            IsPlaying = true;
+            Board = new Tile[height, width];
+            Bombs = Bombs_;
+            FillBoard();
+        }
+        public void FillBoard()
+        {
+            if (Board == null) return;
+            Random rnd=new Random();
+            List<int> heights=new List<int>();
+            List<int> widths=new List<int>();
+            for (int i = 0; i < Bombs; i++)//getting the locarions of bombs
+            {
+                int width = rnd.Next(Board.GetLength(0));
+                int height = rnd.Next(Board.GetLength(1));
+                if (widths.Contains(width) && heights.Contains(height)) i--;
+                else
+                {
+                    widths.Add(width);
+                    heights.Add(height);
+                }
+            }
+            for(int i = 0; i < Board.GetLength(0); i++)//width
+            {
+                for(int j = 0; j < Board.GetLength(1); j++)//height
+                {
+                    Board[i,j] = new Tile(0, false);
+                }
+            }
+            for(int i = 0;i < heights.Count; i++)//filling an empty board with tiles with bombs
+            {
+                List<Tile> adj = new List<Tile>();
+                if (widths[i] > 0 && widths[i] < Board.GetLength(0) - 1)
+                {
+                    if (heights[i] > 0 && heights[i] < Board.GetLength(1) - 1)//if in middle middle
+                    {
+                        
+                    }
+                    else//if in middle but no top or bottom
+                    {
+
+                    }
+                }
+                else
+                {
+                    if (heights[i] > 0 && heights[i] < Board.GetLength(1) - 1)//if in middle left or right
+                    {
+
+                    }
+                    else//if in corner
+                    {
+
+                    }
+                }
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="Case">1-middle middle.2-middle top/bottom.3-right/left middle.4-corner</param>
+        /// <returns></returns>
+        public List<Tile> GetAdj(int width,int height,int Case)
+        {
+            List<Tile> result=new List<Tile>();
+            switch (Case) 
+            {
+                case 1:
+                    for(int i=-1)
+                case 2:
+                case 3:
+                case 4:
+            }
+        }
+        
+        public bool? WonGame()
+        {
+            return null;
+        }
+    }
+}
--- Minesweeper/Tile.cs
+++ Minesweeper/Tile.cs
@@ -8,116 +8,135 @@ namespace Minesweeper
 {
     public class Tile
     {
-        public Tile[] Adjacants { get; private set; }//adjacant tiles
-        public int Value { get;set; }//goes from 0-9, -1 value is a bomb
-        public bool Vailed {  get; set; }//if the tile is vailed this will be true
-        
-        public Tile()
+        public int Value { get; private set; }
+        public bool Unvailed { get; private set; }//-1 is bomb
+        public Tile(int Value_)
         {
-            Adjacants = new Tile[8];
+            Value = Value_;
+            Unvailed = false;
         }
-        public Tile(int value,bool vailed)
+        public Tile(int Value_,bool Unvailed_) : this(Value_)
         {
-            Adjacants = new Tile[8];
-            this.Value = value;
-            this.Vailed = vailed;
+            Unvailed = Unvailed_;
         }
-        /// <summary>
-        /// produces a tile with a given value and a place near the given title
-        /// </summary>
-        /// <param name="tile">an adjacant tile</param>
-        /// <param name="val">the value of the new tile</param>
-        /// <param name="place">the place in which the given tile will be placed in the new tile</param>
-        public Tile(Tile tile,int place)
+        public bool Dig()//returns true when the move doesn't kill you
         {
-            this.Vailed = true;
-            this.Adjacants= new Tile[8];
-            LinkTile(place,tile);
+            if (Unvailed) return true;
+            Unvailed = true;
+            if (Value == -1) return false;
+            return true;
         }
-        public Tile(int height,int width,int bombs)
-        {
-            this.Adjacants = new Tile[8];
-            List<int> bombNum = bombNums(height,width,bombs);
-            Tile left = this;
-            for(int i = 0;i<height;i++)
-            {
-                Tile t = left;
-                for(int j = 0; j < width; j++)
-                {
-                    if (bombNum.Count>0&&bombNum[0] == i*width +j) t.Value = -1;
-                    else t.Value = 0;
-                    t.Vailed= true;
-                    if (bombNum.Count > 0)bombNum.RemoveAt(0);
-                    t.Adjacants[2] = new Tile(t,6);
-                    t = t.Adjacants[2];
-                }
-                left.Adjacants[4] = new Tile(left, 0);
-                left = left.Adjacants[4];
-            }
 
-        }
-        private List<int> bombNums(int height, int width, int bombs)
-        {
-            List<int> result = new List<int>();
-            for(int i=0;i<bombs;i++)
-            {
-                Random rnd = new Random();
-                int add= rnd.Next(0,height*width);
-                while (result.Any(x => x==add)) add = rnd.Next(0, height * width);
-                result.Add(add);
-            }
-            result.OrderDescending();
-            return result;
-        }
-        private void LinkTile(int place,Tile? t)
-        {
-            if (t == null) return;
-            this.Adjacants[place] = t;
-            t.Adjacants[FlipPlace(place)] = this;
-            Tile nextTile=null;
-            switch (place)
-            {
-                case 0: { if (t.Adjacants[2] != null) nextTile = t.Adjacants[2]; break; }
-                case 1: { if (t.Adjacants[4] != null) nextTile = t.Adjacants[4]; break; }
-                case 2: { if (t.Adjacants[4] != null) nextTile = t.Adjacants[4]; break; }
-                case 3: { if (t.Adjacants[6] != null) nextTile = t.Adjacants[6]; break; }
-                case 4: { if (t.Adjacants[6] != null) nextTile = t.Adjacants[6]; break; }
-                case 5: { if (t.Adjacants[0] != null) nextTile = t.Adjacants[0]; break; }
-                case 6: { if (t.Adjacants[0] != null) nextTile = t.Adjacants[0]; break; }
-                case 7: { if (t.Adjacants[2] != null) nextTile = t.Adjacants[2]; break; }
-            }
-            LinkTile((place+1)%8, nextTile);
-        }
-        private int FlipPlace(int place)
-        {
+        //public Tile[] Adjacants { get; private set; }//adjacant tiles
+        //public int Value { get;set; }//goes from 0-9, -1 value is a bomb
+        //public bool Vailed {  get; set; }//if the tile is vailed this will be true
+        
+        //public Tile()
+        //{
+        //    Adjacants = new Tile[8];
+        //}
+        //public Tile(int value,bool vailed)
+        //{
+        //    Adjacants = new Tile[8];
+        //    this.Value = value;
+        //    this.Vailed = vailed;
+        //}
+        ///// <summary>
+        ///// produces a tile with a given value and a place near the given title
+        ///// </summary>
+        ///// <param name="tile">an adjacant tile</param>
+        ///// <param name="val">the value of the new tile</param>
+        ///// <param name="place">the place in which the given tile will be placed in the new tile</param>
+        //public Tile(Tile tile,int place)
+        //{
+        //    this.Vailed = true;
+        //    this.Adjacants= new Tile[8];
+        //    LinkTile(place,tile);
+        //}
+        //public Tile(int height,int width,int bombs)
+        //{
+        //    this.Adjacants = new Tile[8];
+        //    List<int> bombNum = bombNums(height,width,bombs);
+        //    Tile left = this;
+        //    for(int i = 0;i<height;i++)
+        //    {
+        //        Tile t = left;
+        //        for(int j = 0; j < width; j++)
+        //        {
+        //            if (bombNum.Count>0&&bombNum[0] == i*width +j) t.Value = -1;
+        //            else t.Value = 0;
+        //            t.Vailed= true;
+        //            if (bombNum.Count > 0)bombNum.RemoveAt(0);
+        //            t.Adjacants[2] = new Tile(t,6);
+        //            t = t.Adjacants[2];
+        //        }
+        //        left.Adjacants[4] = new Tile(left, 0);
+        //        left = left.Adjacants[4];
+        //    }
+
+        //}
+        //private List<int> bombNums(int height, int width, int bombs)
+        //{
+        //    List<int> result = new List<int>();
+        //    for(int i=0;i<bombs;i++)
+        //    {
+        //        Random rnd = new Random();
+        //        int add= rnd.Next(0,height*width);
+        //        while (result.Any(x => x==add)) add = rnd.Next(0, height * width);
+        //        result.Add(add);
+        //    }
+        //    result.OrderDescending();
+        //    return result;
+        //}
+        //private void LinkTile(int place,Tile? t)
+        //{
+        //    if (t == null) return;
+        //    this.Adjacants[place] = t;
+        //    t.Adjacants[FlipPlace(place)] = this;
+        //    Tile nextTile=null;
+        //    switch (place)
+        //    {
+        //        case 0: { if (t.Adjacants[2] != null) nextTile = t.Adjacants[2]; break; }
+        //        case 1: { if (t.Adjacants[4] != null) nextTile = t.Adjacants[4]; break; }
+        //        case 2: { if (t.Adjacants[4] != null) nextTile = t.Adjacants[4]; break; }
+        //        case 3: { if (t.Adjacants[6] != null) nextTile = t.Adjacants[6]; break; }
+        //        case 4: { if (t.Adjacants[6] != null) nextTile = t.Adjacants[6]; break; }
+        //        case 5: { if (t.Adjacants[0] != null) nextTile = t.Adjacants[0]; break; }
+        //        case 6: { if (t.Adjacants[0] != null) nextTile = t.Adjacants[0]; break; }
+        //        case 7: { if (t.Adjacants[2] != null) nextTile = t.Adjacants[2]; break; }
+        //    }
+        //    LinkTile((place+1)%8, nextTile);
+        //}
+        //private int FlipPlace(int place)
+        //{
             
-            return (place+4)%8;
-        }
-        public override string ToString()
-        {
-            Tile t = this;
-            while(t!=null&&(t.Adjacants[1] != null && t.Adjacants[2] != null && t.Adjacants[3] != null && t.Adjacants[4] != null && t.Adjacants[5] != null))
-            {
-                t = t.Adjacants[6];
-            }
-            while (t != null && (t.Adjacants[7] != null && t.Adjacants[0] != null && t.Adjacants[1] != null && t.Adjacants[2] != null && t.Adjacants[3] != null))
-            {
-                t = t.Adjacants[0];
-            }
-            string result = "";
-            Tile left = t;
-            while (left != null)
-            {
-                while (t != null)
-                {
-                    result += t.Value + " ";
-                    t = t.Adjacants[2];
-                }
-                result+= "\n";
-                left = left.Adjacants[4];
-                t = left;
-            }
-            return result;
-        }
+        //    return (place+4)%8;
+        //}
+        //public override string ToString()
+        //{
+        //    Tile t = this;
+        //    while(t!=null&&(t.Adjacants[1] != null && t.Adjacants[2] != null && t.Adjacants[3] != null && t.Adjacants[4] != null && t.Adjacants[5] != null))
+        //    {
+        //        t = t.Adjacants[6];
+        //    }
+        //    while (t != null && (t.Adjacants[7] != null && t.Adjacants[0] != null && t.Adjacants[1] != null && t.Adjacants[2] != null && t.Adjacants[3] != null))
+        //    {
+        //        t = t.Adjacants[0];
+        //    }
+        //    string result = "";
+        //    Tile left = t;
+        //    while (left != null)
+        //    {
+        //        while (t != null)
+        //        {
+        //            result += t.Value + " ";
+        //            t = t.Adjacants[2];
+        //        }
+        //        result+= "\n";
+        //        left = left.Adjacants[4];
+        //        t = left;
+        //    }
+        //    return result;
+        //}
     }
 }
